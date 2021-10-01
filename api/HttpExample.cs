59db10512c2d313e64c0ca496c1c36c43ab79576
Kamesh Tanneru <kamesrao@microsoft.com>
2021-10-01 10:00:00 +0530

    initialize dotnet-60 canary app

---
+++ api/HttpExample.cs
@@ -0,0 +1,27 @@
+namespace dotnet_60
+{
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Azure.WebJobs;
+    using Microsoft.Azure.WebJobs.Extensions.Http;
+    using Microsoft.Extensions.Logging;
+
+    public static class HttpExample
+    {
+        [FunctionName("HttpExample")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string date = "2021-10-01 10:00:00";
+
+            return new OkObjectResult(date + "one two three");
+        }
+    }
+}
